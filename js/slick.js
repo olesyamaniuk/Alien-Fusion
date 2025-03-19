3b6af193c23abc99d4dec7bd52b7c76c094167a1
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-03-19 14:16:22 +0200

    1

---
+++ js/slick.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(".reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    centerMode: true,
+    cssEase: "linear",
+  });
+});
